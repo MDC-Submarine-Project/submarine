a079dac3774c30a13c9f505be60ac416cbf92024
sebasphere <sebasphereemails@gmail.com>
2025-10-15 22:34:19 -0400

    chore: add concurrent runner task for client and server with shared output

--- client/src/main/java/dev/senj/client/ClientMain.java
+++ client/src/main/java/dev/senj/client/ClientMain.java
@@ -5,5 +5,15 @@ import dev.senj.common.Utils;
 public class ClientMain {
     public static void main(String[] args) {
         System.out.println(Utils.greeting("Client"));
+        while (true) {
+            System.out.println("client: still running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("client interrupted, exiting.");
+                break;
+            }
+        }
     }
 }
--- server/src/main/java/dev/senj/server/ServerMain.java
+++ server/src/main/java/dev/senj/server/ServerMain.java
@@ -5,5 +5,15 @@ import dev.senj.common.Utils;
 public class ServerMain {
     public static void main(String[] args) {
         System.out.println(Utils.greeting("Server"));
+        while (true) {
+            System.out.println("server: still running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("server interrupted, exiting.");
+                break;
+            }
+        }
     }
 }
